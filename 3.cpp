430683170fb25cbd2135f2ac8508eca91265301e
Erik <78443570+thekhachik@users.noreply.github.com>
2021-10-28 13:45:03 +0300

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	double a, b, c, d, e, f, g, h, i, j;
+	cin >> a >> b >> c >> d >> e >> f >> g >> h >> i >> j;
+	{
+		ofstream F1("f.txt");
+		F1 << a << " " << b << " " << c << " " << d << " " << e << " " << f << " " << g << " " << h << " " << i << " " << j;
+		F1.close();
+	}
+	ifstream F2("f.txt");
+	do
+	{
+		F2 >> a >> b >> c >> d >> e >> f >> g >> h >> i >> j;
+	} 
+	while (!F2.eof());
+	cout << a + b + c + d + e + f + g + h + i + j << endl;
+	F2.close();
+	return 0;
+}
