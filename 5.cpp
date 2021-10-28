73ab944caa0679f621ac199aec8fa52e93154077
Erik <78443570+thekhachik@users.noreply.github.com>
2021-10-28 13:46:02 +0300

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+double a, b, c, h, r;
+
+double PloshadPR(double a, double b)
+{
+	double result = a * b;
+	return result;
+}
+double PloshadTR(double a, double h)
+{
+	double result1 = (a * h) / 2.0;
+	return result1;
+}
+double PloshadKR(double r)
+{
+	double result2 = 3.14 * r * r;
+	return result2;
+}
+int main()
+{
+	cin >> a >> b >> c >> h >> r;
+	double result = PloshadPR(a, b);
+	double result1 = PloshadTR(a, h);
+	double result2 = PloshadKR(r);
+	cout << result << " " << result1 << " " << result2 << endl;
+}
